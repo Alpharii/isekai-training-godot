ecc04130ded16dabdd3bb6cc3d900c0dbe46b978
alpharii <alphari404@gmail.com>
2026-06-05 17:49:29 +0700

    initial commit

---
+++ resources/data/player-data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name PlayerData;
+
+@export var strength := 10;
+@export var endurance := 10;
+@export var magic := 10;
+@export var wisdom := 10;
+@export var dexterity := 10;
+@export var agility := 10;
+
+@export var turn := 1;
